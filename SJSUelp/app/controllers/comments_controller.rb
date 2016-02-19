e667c93f106b17ca1540e54625f720f609745fa5
Duy Ngo <duy_ngo94@yahoo.com>
2016-02-18 21:06:47 -0800

    Comment Display added
    Post > Show > Option to comment on posts added and displays comments for
    the post.

--- SJSUelp/app/controllers/comments_controller.rb
+++ SJSUelp/app/controllers/comments_controller.rb
@@ -1,15 +1,18 @@
 class CommentsController < ApplicationController
-  def create
-    @post = Post.find(params[:post_id])
-    if @post.comments.create(comment_params)
-      redirect_to @post,
-                  notice: 'Comment was successfully created.'
-      redirect_to @post,
-else
-end end
+	def create
+		@post = Post.find(params[:post_id])
+		if @post.comments.create(comment_params)
+		  redirect_to post_path(@post)
+		  flash.notice= 'Comment was successfully created.'
+		 
+		else
+		    redirect_to post_path(@post)
+			flash.alert= 'Error creating comment'
+			flash.alert= 'Please fill out the required fields'
+		end 
+	end
 private
-alert: 'Error creating comment'
-  def comment_params
-    params.require(:comment).permit(:author, :body)
-end
+	def comment_params
+		params.require(:comment).permit(:author, :body)
+  end
 end
