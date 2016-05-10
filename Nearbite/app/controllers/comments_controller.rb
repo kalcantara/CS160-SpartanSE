c964ef34c658837f47cdc9430f9553a318a12cd3
Duy Ngo <duy.ngo94@gmail.com>
2016-05-09 18:58:32 -0700

    Update to Comment functionality
    Displays author of comments.

--- Nearbite/app/controllers/comments_controller.rb
+++ Nearbite/app/controllers/comments_controller.rb
@@ -6,6 +6,6 @@ class CommentsController < ApplicationController
   end
 private
   def comment_params
-    params.require(:comment).permit(:body)
+    params.require(:comment).permit(:user ,:body)
 end
 end
